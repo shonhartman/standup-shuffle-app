d2c6455c03a024dfa727f2fc0d4f5bd50c19b58b
Shaun Hartman <shaunh@teamsi.com>
2022-02-11 16:05:49 -0600

    first branch edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Let's build an app!
+          Let's build an app! Now we are going to build our components.
         </p>
         <a
           className="App-link"
