c7541e31ec64639b81ca7b1fd5d6ab036c086237
Shaun Hartman <shaunh@teamsi.com>
2022-02-14 18:00:57 -0600

    refactor to use React Hooks

--- src/App.js
+++ src/App.js
@@ -1,61 +1,59 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Developer from './components/Developer';
 import AddDeveloperForm from './components/AddDeveloperForm';
 
-class App extends Component {
+function App() {
 
-  state = {
-    developers: [
-      {
-        name: "Jonathan",
-        id: 1
-      },
-      {
-        name: "Jereme",
-        id: 2
-      },
-      {
-        name: "JPG",
-        id: 3
-      },
-      {
-        name: "Reehana",
-        id: 4
-      }
-    ]
+  // TODO : get initial state from localStorage
+  const [developers, setDevelopers] = useState([
+    {
+      name: "Jonathan",
+      id: 1
+    },
+    {
+      name: "Jereme",
+      id: 2
+    },
+    {
+      name: "JPG",
+      id: 3
+    },
+    {
+      name: "Reehana",
+      id: 4
+    }
+  ]);
+
+  // TODO: Persist to localStorage with a similar function to below
+  // localStorage.setItem('CURRENT_DEVS', JSON.stringify(this.state.developers));
+  function useStickyState(defaultValue, key) {
+    const [value, setValue] = useState(() => {
+      const stickyValue = window.localStorage.getItem(key);
+      return stickyValue !== null
+        ? JSON.parse(stickyValue)
+        : defaultValue;
+    });
+    useEffect(() => {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    return [value, setValue];
+  }
+
+  const handleRemoveDeveloper = (id) => {
+    setDevelopers(developers.filter(d => d.id !== id));
   };
 
-  handleRemoveDeveloper = (id) => {
-    this.setState( prevState => {
-      return {
-        developers: prevState.developers.filter( p => p.id !== id)
-      }
-    })
-  };
+  const handleAddDeveloper = (name) => {
+    setDevelopers([...developers, {
+      name,
+      id: Date.now()
+    }]);
+  }
 
-  // developers id counter
-  prevDeveloperId = 4;
-
-  handleAddDeveloper = (name) => {
-    this.setState(prevState => {
-      return {
-        developers: [
-          ...prevState.developers,
-          {
-            name,
-            score: 0,
-            id: this.prevDeveloperId += 1
-          }
-        ]
-      }
-    })
-  };
-
-  shuffleDevs = () => {
-    this.setState(prevState => {
-      let currentIndex = prevState.developers.length,  randomIndex;
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
 
       // While there remain elements to shuffle...
       while (currentIndex !== 0) {
@@ -65,38 +63,41 @@ class App extends Component {
         currentIndex--;
         
         // And swap it with the current element.
-        [prevState.developers[currentIndex], prevState.developers[randomIndex]] = [
-          prevState.developers[randomIndex], prevState.developers[currentIndex]];
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
       }
 
-      return {
-        developers: prevState.developers 
-      }
-    })
+      return array;
   }
 
-  render() {
-    return (
-      <div className="board">
-        <Header 
-          title="Standup Shuffle" 
-          developers={this.state.developers} 
-          shuffleDevs={this.shuffleDevs}
-        />
-        {/* DEVELOPER LIST */}
-        {this.state.developers.map( (developer, index) => 
-          <Developer 
-            key={developer.name} 
-            id={developer.id}
-            name={developer.name} 
-            index={index}
-            removeDeveloper={this.handleRemoveDeveloper} 
-          /> 
-          )}
-          <AddDeveloperForm addDeveloper={this.handleAddDeveloper} />
-      </div>
-    );
+  const shuffleDevs = () => {
+
+    const shuffledDevs = [...developers];
+
+    setDevelopers(shuffle(shuffledDevs));
+
+  };
+
+  return (
+    <div className="board">
+      <Header 
+        title="Standup Shuffle" 
+        developers={developers} 
+        shuffleDevs={shuffleDevs}
+      />
+      {/* DEVELOPER LIST */}
+      {developers.map( (developer, index) => 
+        <Developer 
+          key={developer.name} 
+          id={developer.id}
+          name={developer.name} 
+          index={index}
+          removeDeveloper={handleRemoveDeveloper} 
+        /> 
+      )}
+      <AddDeveloperForm addDeveloper={handleAddDeveloper} />
+    </div>
+  );
   }
-}
 
 export default App;
